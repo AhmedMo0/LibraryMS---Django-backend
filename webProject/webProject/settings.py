c8b4343b7e4eebb0c764f374d24730d034a2fc86
unknown <ahmedmo@stud.cu.edu.eg>
2022-02-14 06:24:12 +0200

    update settings v2

--- webProject/webProject/settings.py
+++ webProject/webProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['infinite-springs-37521.herokuapp.com/', '127.0.0.1']
 
